da1f69960a83bbfe88924744c4fe148e3e6d3709
Andrea055 <vivanco11.ac@gmail.com>
2023-03-09 17:34:29 +0000

    add matrix

--- lib/parse_file.py
+++ lib/parse_file.py
@@ -1,4 +1,4 @@
-def open_line(filename):
+def open_line_snacks(filename):
     with open(filename) as file:
         i = 0
         for line in file:
@@ -8,6 +8,32 @@ def open_line(filename):
                 i += 1
                 continue
 
+def open_line_matrix(filename):
+    matrix_raw = []
+    with open(filename) as file:
+        i = 0
+        for line in file:
+            if i > 1:
+                matrix_raw.append(line)
+            i += 1
+        return matrix_raw
+
 def parse_snacks(line):
     snacks_raw = line.split(" ")
     return snacks_raw
+
+def parse_matrix(lines):
+    matrix = []
+    for line in lines:
+        matrix.append(line.split(" "))
+    
+    i = 0
+    j = 0
+    for matrix_element in matrix:
+        for line in matrix_element:
+            matrix[i][j] = matrix[i][j].replace("\n", "")
+            j += 1
+        j = 0
+        i += 1
+
+    return matrix
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import lib.parse_file as parser
+
+lines_matrix = parser.open_line_matrix("example.txt")
+lines_snacks = parser.open_line_snacks("example.txt")
+snacks = parser.parse_snacks(lines_snacks)
+matrix = parser.parse_matrix(lines_matrix)
